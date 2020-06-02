34632cb28c273178f15f51917f9806eed22d9bd2
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-06-02 18:56:40 +0530

    Add basic functions

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//to create more functions used a class
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name:'Sri Lanka'
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.name}
+          </a><br/>
+          <button onClick={() => this.setState({name:'India'})}>Change Name</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
