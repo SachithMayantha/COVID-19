7ef0bf4a81ff194141695ac2b1a751c2bc1f52a4
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-06-04 19:03:01 +0530

    Updates

---
+++ precache-manifest.3b6403e6abd4089de3bc8606a9bfa7ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e38cd42537bd0f5b07a9160634ccc3",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "1a9c631454810e235d99",
+    "url": "/COVID-19/static/css/main.f6fc3bbb.chunk.css"
+  },
+  {
+    "revision": "e4ada01bf305d3309a93",
+    "url": "/COVID-19/static/js/2.6007c404.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/COVID-19/static/js/2.6007c404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9c631454810e235d99",
+    "url": "/COVID-19/static/js/main.f03e5ad2.chunk.js"
+  },
+  {
+    "revision": "c895d1151fce13e00654",
+    "url": "/COVID-19/static/js/runtime-main.fe56b5bc.js"
+  },
+  {
+    "revision": "a6f6644adb094eb2d4343f151de08aa6",
+    "url": "/COVID-19/static/media/banner.a6f6644a.png"
+  }
+]);
