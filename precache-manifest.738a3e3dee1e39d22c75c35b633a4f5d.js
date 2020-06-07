4cb14ed48b13ad3f7e836e5e9939503e2d35317e
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-06-07 17:09:55 +0530

    Updates

---
+++ precache-manifest.738a3e3dee1e39d22c75c35b633a4f5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f258182a6d194c9f2d13db7b3cab5a1",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "89c91ec69cae53b05f9a",
+    "url": "/COVID-19/static/css/main.6be67a3b.chunk.css"
+  },
+  {
+    "revision": "e4ada01bf305d3309a93",
+    "url": "/COVID-19/static/js/2.6007c404.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/COVID-19/static/js/2.6007c404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c91ec69cae53b05f9a",
+    "url": "/COVID-19/static/js/main.93b3e1d9.chunk.js"
+  },
+  {
+    "revision": "c895d1151fce13e00654",
+    "url": "/COVID-19/static/js/runtime-main.fe56b5bc.js"
+  },
+  {
+    "revision": "a6f6644adb094eb2d4343f151de08aa6",
+    "url": "/COVID-19/static/media/banner.a6f6644a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19/precache-manifest.251cccd9aa87a470b1d5f085b1ed0396.js"
+  "/COVID-19/precache-manifest.738a3e3dee1e39d22c75c35b633a4f5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
